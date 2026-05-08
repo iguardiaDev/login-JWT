66b840b03db652ce29823d253ab5193de7c05c93
iguardiaDev <danieliguardia5@gmail.com>
2026-05-07 20:35:43 -0600

    Agregamos ruta raiz con info de la API

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ app.use(cors());
 app.use(express.json());
 app.use('/usuarios', routes);
 
+app.get('/', (req, res) => {
+  res.json({
+    mensaje: 'API de autenticación con JWT',
+    version: '1.0',
+    endpoints: [
+      'POST /usuarios/registro',
+      'POST /usuarios/login',
+      'GET /usuarios/perfil'
+    ]
+  });
+});
+
 app.listen(3000, function()
 {
     console.log('Servidor corriendo en el puerto 3000');
